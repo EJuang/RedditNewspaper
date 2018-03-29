95ce3b8109b49b62a9b3e8e7537d4e894a74ef37
Eric Juang <ericjuang0@gmail.com>
2018-03-29 17:47:30 -0400

    AJAX pulling headlines, photos

---
+++ RedditNewspaper/Content/redditapi.js
@@ -0,0 +1,23 @@
+﻿
+$(document).ready(function () {
+
+    var urlString = "https://www.reddit.com/r/usnews.json";
+
+    $.ajax({
+        url: urlString,
+        dataType: "json"
+    }).done(function (response) {
+
+        for (i = 0; i < 5; i++) {
+            var headline = response.data.children[i].data.title;
+            var imgUrl = response.data.children[i].data.preview.images[0].source.url;
+            $("#headlines").append("<div>");
+            $("#headlines").append('<img class="thumbnail" src="' + imgUrl + '"/>');
+            $("#headlines").append("<p>" + headline + "</p>");
+            $("#headlines").append("</div>");
+
+        }
+    });
+
+
+});
